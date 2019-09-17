b13d7b89f9616e3ba351c414c163d2f31264f301
Gavin Fynbo <gavfynbo@gmail.com>
2019-09-16 19:23:19 -0700

    add get route

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
+});
+
 const {PORT} = process.env;
 app.listen(PORT, () => console.log(`Wizardry happening on port ${PORT}`));
