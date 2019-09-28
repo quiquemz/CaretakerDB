f868c72d8386a9dd7b971b78930121d7f99a6626
Gavin Fynbo <gavfynbo@gmail.com>
2019-09-28 15:13:59 -0700

    add frontend and add passport

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-require('dotenv').config();
-require('./server/db-conn');
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
+const passport = require("passport");
+const users = require("./server/routes/api/users");
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "client/build")));
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-// mount routes
-app.use('/api/contracts/', require('./server/routes/contracts-route'));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-});
-
-const {PORT} = process.env;
-app.listen(PORT, () => console.log(`Wizardry happening on port ${PORT}`));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
