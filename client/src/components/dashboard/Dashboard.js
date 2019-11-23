02d51be42b3c93c072d65a456e4990f4648cc3fb
Gavin Fynbo <gavfynbo@gmail.com>
2019-11-22 21:47:07 -0800

    update dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -12,19 +12,19 @@ class Dashboard extends Component {
       contracts: [],
     };
   }
-    onLogoutClick = e => {
-        e.preventDefault();
-        this.props.logoutUser();
-    };
-    componentDidMount() {
-      fetch('/contracts/' + this.props.auth.user.id)
-        .then(response => response.json())
-        .then(data => { 
-          this.setState({ contracts: data });
-          console.log(data);
-        });
-    };
-    render() {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+  componentDidMount() {
+    fetch('/contracts/' + this.props.auth.user.id)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ contracts: data });
+        console.log(data);
+      });
+  };
+  render() {
     const { user } = this.props.auth;
     const { contracts } = this.state;
     return (
@@ -34,23 +34,23 @@ class Dashboard extends Component {
           direction="column"
           spacing={3}>
           <Grid item>
-            { user.firstName ? <Typography variant="h3">Welcome, {user.firstName}.</Typography>
-              : <Typography variant="h3">Welcome to your Dashboard!</Typography> }
+            {user.firstName ? <Typography variant="h3">Welcome, {user.firstName}.</Typography>
+              : <Typography variant="h3">Welcome to your Dashboard!</Typography>}
             <Typography variant="body1" gutterBottom>This is the dashboard, you can find everything related to the management of your caretaking contracts here.</Typography>
           </Grid>
           <Grid item>
-            { contracts ?
+            {contracts ?
               <Grid container spacing={3}> {
-              contracts.map((contract) =>
-                <Grid item xs={6} sm={3}>
-                  <Contract contract={contract} />
-                </Grid>
-              )
-            }
-            </Grid> : <Contract loading/>
+                contracts.map(contract =>
+                  <Grid item xs={6} sm={3} key={contract.id}>
+                    <Contract contract={contract} key={contract.id} />
+                  </Grid>
+                )
+              }
+              </Grid> : <Contract loading />
             }
           </Grid>
-            
+
         </Grid>
       </Container>
     );
