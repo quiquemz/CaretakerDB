c93763e4ab7aa187051fb077ea48dce4fde1b060
Gavin Fynbo <gavfynbo@gmail.com>
2019-11-22 19:08:16 -0800

    Actually add correct support for non redux devtools ext here

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,12 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = typeof window === 'object' || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
   )
 );
 export default store;
