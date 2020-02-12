abd7bf19cd6b913ca1c3fbd9c13e73677afed4ff
Gavin Fynbo <gavfynbo@gmail.com>
2020-02-12 11:39:58 -0800

    Add key for api

--- server/routes/api/registrationKey.js
+++ server/routes/api/registrationKey.js
@@ -1,21 +1,31 @@
 const router = require('express').Router();
 const RegistrationKey = require('../../models/RegistrationKey');
 
-router.get('/', (req, res, next) => {
+router.get('/:secret', (req, res, next) => {
     console.log("Accessing registration keys ");
-    RegistrationKey.find({}, (err, keys) => {
+    const isCorrect = req.params.secret === (process.env.REG_SECRET || require("../../../config/keys").REG_SECRET);
+    if (isCorrect) {
+      RegistrationKey.find({}, (err, keys) => {
         if (err) next(err);
         else res.json(keys);
-    });
+      });
+    } else {
+      res.status(401).json({ msg: 'Incorrect registration secret.' });
+    }
 });
 
-router.get('/add/:key', (req, res, next) => {
+router.get('/add/:secret/:key', (req, res, next) => {
     const key = req.params.key;
-    const newKey = new RegistrationKey({key: key});
-    newKey.save(err => {
-        if (err) next(err);
-        else res.json({ newKey, msg: 'key successfully saved' });
-    });
+    const isCorrect = req.params.secret === (process.env.REG_SECRET || require("../../../config/keys").REG_SECRET);
+    if (isCorrect) {
+      const newKey = new RegistrationKey({key: key});
+      newKey.save(err => {
+          if (err) next(err);
+          else res.json({ newKey, msg: 'key successfully saved' });
+      });
+    } else {
+      res.status(401).json({ msg: 'Incorrect registration secret.' });
+    }
 });
 
 
