e97710e8ab1fdf0a36b497fa34b3318468cae2fd
Gavin Fynbo <gavfynbo@gmail.com>
2020-02-11 21:14:49 -0800

    Heroku changes

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const app = express();
 const passport = require("passport");
 const users = require("./server/routes/api/users");
 const properties = require("./server/routes/api/properties");
+const path = require('path');
 const db = require("./config/keys").mongoURI;
 const dbLocal = require("./config/keys").localMongoURI;
 
 
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Bodyparser middleware
 app.use(Cors());
@@ -25,7 +28,7 @@ app.use(passport.initialize());
 // Connect to MongoDB
 mongoose
   .connect(
-    dbLocal,
+    db,
     { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB successfully connected"))
@@ -37,5 +40,9 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/properties", properties);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
