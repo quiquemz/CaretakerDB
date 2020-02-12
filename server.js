bc04599164f81b277e0668a42d8217cf63a04f71
Gavin Fynbo <gavfynbo@gmail.com>
2020-02-11 22:07:45 -0800

    Fix heroku

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const passport = require("passport");
 const users = require("./server/routes/api/users");
 const properties = require("./server/routes/api/properties");
 const path = require('path');
-const db = require("./config/keys").mongoURI;
-const dbLocal = require("./config/keys").localMongoURI;
 
-
+const db = process.env.mongoURI;
 const port = process.env.PORT || 5000;
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Bodyparser middleware
 app.use(Cors());
@@ -41,8 +37,12 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/properties", properties);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
